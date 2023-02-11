06ba8490900974ee452ac546dc17f3c9bfef9a70
Rafał Krzyważnia <r.krzywaznia@gmail.com>
2023-02-12 00:05:33 +0100

    push

--- lua/pareto_nvim.lua
+++ lua/pareto_nvim.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: no-unknown
 local M = {}
 
 local opening_parens = {
@@ -22,23 +23,19 @@ M.backward_node = function()
 	local cur = vim.api.nvim_win_get_cursor(0)
 	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
 	while node do
-		---@diagnostic disable-next-line: no-unknown
 		local row, col, _ = node:start()
 		if cur[1] ~= row + 1 or cur[2] ~= col then
 			vim.api.nvim_win_set_cursor(0, { row + 1, col })
 			return
 		end
-		---@diagnostic disable-next-line: no-unknown
 		local sibling = node:prev_sibling()
 		if sibling then
-			---@diagnostic disable-next-line: no-unknown
 			row, col, _ = sibling:start()
 			if cur[1] ~= row + 1 or cur[2] ~= col then
 				vim.api.nvim_win_set_cursor(0, { row + 1, col })
 				return
 			end
 		end
-		---@diagnostic disable-next-line: no-unknown
 		node = node:parent()
 	end
 end
@@ -46,7 +43,6 @@ end
 -- go forward node
 M.forward_node = function()
 	local cur = vim.api.nvim_win_get_cursor(0)
-	---@diagnostic disable-next-line: no-unknown
 	local node, shift
 	if vim.api.nvim_get_mode().mode == "n" then
 		node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
@@ -56,40 +52,37 @@ M.forward_node = function()
 		shift = 0
 	end
 	while node do
-		---@diagnostic disable-next-line: no-unknown
 		local row, col, _ = node:end_()
 		if cur[1] ~= row + 1 or cur[2] ~= col - shift then
 			vim.api.nvim_win_set_cursor(0, { row + 1, col - shift })
 			return
 		end
-		---@diagnostic disable-next-line: no-unknown
 		local sibling = node:next_sibling()
 		if sibling then
-			---@diagnostic disable-next-line: no-unknown
 			row, col, _ = sibling:end_()
 			if cur[1] ~= row + 1 or cur[2] ~= col - shift then
 				vim.api.nvim_win_set_cursor(0, { row + 1, col - shift })
 				return
 			end
 		end
-		---@diagnostic disable-next-line: no-unknown
 		node = node:parent()
 	end
 end
 
--- wrap treesitter node with
----@param char string
-M.wrap_node = function(char)
+-- wrap treesitter node with char
+---@ char string
+---@param node? any
+M.wrap_node = function(char, node)
 	local cur = vim.api.nvim_win_get_cursor(0)
-	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
+	if node == nil then
+		node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
+	end
 	if node then
 		---@diagnostic disable-next-line: param-type-mismatch
 		local node_text = vim.treesitter.query.get_node_text(node, 0, {})
 		---@diagnostic disable-next-line: param-type-mismatch
 		node_text = vim.split(node_text, "\n", {})
-		---@diagnostic disable-next-line: no-unknown
 		local start_row, start_col, end_row, end_col = node:range()
-		---@diagnostic disable-next-line: no-unknown
 		local begin_char, end_char
 		if opening_parens[char] ~= nil then
 			begin_char = char
@@ -98,13 +91,40 @@ M.wrap_node = function(char)
 			node_text[1] = begin_char .. node_text[1]
 			node_text[#node_text] = node_text[#node_text] .. end_char
 		end
+		if closing_parens[char] ~= nil then
+			begin_char = closing_parens[char]
+			--- @type string
+			end_char = char
+			node_text[1] = begin_char .. " " .. node_text[1]
+			node_text[#node_text] = node_text[#node_text] .. " " .. end_char
+		end
 		vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, node_text)
 		vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col + 1 })
 	end
 end
 
-vim.keymap.set("n", "<F7>", function()
-	M.wrap_node("(")
-end)
+-- wrap treesitter parent node with cahr
+---@param char string
+M.wrap_parent_node = function(char)
+	local cur = vim.api.nvim_win_get_cursor(0)
+	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
+	if node then
+		node = node:parent()
+		M.wrap_node(char, node)
+	end
+end
+
+-- wrap treesitter node with char
+---@param char string
+M.wrap_current_node = function(char)
+	local cur = vim.api.nvim_win_get_cursor(0)
+	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
+	if node then
+		local parent = node:parent()
+		if parent then
+			M.wrap_node(char, parent)
+		end
+	end
+end
 
 return M
