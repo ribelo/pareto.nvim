ce87617eceb2d5325c82cc5a76bb4edf995ac915
Rafał Krzyważnia <r.krzywaznia@gmail.com>
2023-02-19 17:58:06 +0100

    polish code

--- lua/pareto_nvim.lua
+++ lua/pareto_nvim.lua
@@ -1,6 +1,14 @@
 ---@diagnostic disable: no-unknown
 local M = {}
 
+M.default = {
+	hl = "IncSearch",
+}
+
+M.setup = function(opts)
+	M.default = vim.tbl_deep_extend("force", M.default, opts)
+end
+
 local opening_parens = {
 	["("] = ")",
 	["["] = "]",
@@ -209,6 +217,33 @@ M.wrap_current_node = function(char)
 end
 
 -- raise current treesitter node and replace parent node
+M.raise_node = function()
+	local cur = vim.api.nvim_win_get_cursor(0)
+	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
+	vim.pretty_print("begin")
+	while node do
+		local node_row_start, node_col_start, _ = node:start()
+		local node_row_end, node_col_end, _ = node:end_()
+		local char_start =
+			vim.api.nvim_buf_get_text(0, node_row_start, node_col_start, node_row_start, node_col_start + 1, {})[1]
+		local char_end = vim.api.nvim_buf_get_text(0, node_row_end, node_col_end - 1, node_row_end, node_col_end, {})[1]
+		vim.pretty_print("start", char_start, "end", char_end)
+		if opening_parens[char_start] and closing_parens[char_end] then
+			vim.api.nvim_buf_set_text(0, node_row_start, node_col_start, node_row_start, node_col_start + 1, { "" })
+			vim.api.nvim_buf_set_text(0, node_row_end, node_col_end - 2, node_row_end, node_col_end - 1, { "" })
+			local ns_id =
+				vim.api.nvim_buf_add_highlight(0, 0, M.default.hl, node_row_start, node_col_start, node_col_start + 1)
+			vim.api.nvim_buf_add_highlight(0, ns_id, M.default.hl, node_row_end, node_col_end - 3, node_col_end - 2)
+			vim.defer_fn(function()
+				vim.api.nvim_buf_clear_namespace(0, ns_id, node_row_start, node_row_end + 1)
+			end, 250)
+			return
+		end
+		node = node:parent()
+	end
+end
+
+-- splice current treesitter node
 M.splice_node = function()
 	local cur = vim.api.nvim_win_get_cursor(0)
 	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
@@ -216,19 +251,6 @@ M.splice_node = function()
 		local parent = node:parent()
 		if parent then
 			local parent_start_row, parent_start_col, parent_end_row, parent_end_col = parent:range()
-			---@diagnostic disable-next-line: param-type-mismatch
-			local node_text = vim.treesitter.query.get_node_text(node, 0, {})
-			---@diagnostic disable-next-line: param-type-mismatch
-			local node_text_list = vim.split(node_text, "\n", {})
-			vim.api.nvim_buf_set_text(
-				0,
-				parent_start_row,
-				parent_start_col,
-				parent_end_row,
-				parent_end_col,
-				node_text_list
-			)
-			vim.api.nvim_win_set_cursor(0, { parent_start_row + 1, parent_start_col })
 		end
 	end
 end
@@ -336,7 +358,7 @@ M.comment_node = function()
 			vim.api.nvim_buf_set_lines(0, end_row, end_row + 1, false, { x, string.rep(" ", start_col) .. y })
 		end
 		for i = start_row, end_row do
-			local line = vim.api.nvim_buf_get_lines(0, i, i + 1, true)[1]:sub(start_col)
+			line = vim.api.nvim_buf_get_lines(0, i, i + 1, true)[1]:sub(start_col)
 			vim.api.nvim_buf_set_lines(
 				0,
 				i,
@@ -358,18 +380,16 @@ M.forward_slurp = function()
 		if closing_parens[char] ~= nil then
 			vim.api.nvim_buf_set_text(0, node_row, node_col - 1, node_row, node_col, { "" })
 			local sibling_node = node:next_sibling()
-			local sibling_row, sibling_col, _ = sibling_node:end_()
-			vim.api.nvim_buf_set_text(0, sibling_row, sibling_col, sibling_row, sibling_col, { char })
-			local clients = vim.lsp.get_active_clients({ bufnr = 0 })
-
-			---@param client table
-			for _, client in ipairs(clients) do
-				if client.supports_method("textDocument/formatting") then
-					vim.lsp.buf.format({})
-					break
-				end
+			if sibling_node then
+				local sibling_row, sibling_col, _ = sibling_node:end_()
+				vim.api.nvim_buf_set_text(0, sibling_row, sibling_col, sibling_row, sibling_col, { char })
+				local ns_id =
+					vim.api.nvim_buf_add_highlight(0, 0, M.default.hl, sibling_row, sibling_col, sibling_col + 1)
+				vim.defer_fn(function()
+					vim.api.nvim_buf_clear_namespace(0, ns_id, sibling_row, sibling_row + 1)
+				end, 250)
+				return
 			end
-			return
 		end
 		node = node:parent()
 	end
@@ -381,30 +401,84 @@ M.backward_slurp = function()
 	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2] - 1, {})
 	while node do
 		local node_row, node_col, _ = node:start()
-		vim.pretty_print("node", node_row, node_col)
 		local char = vim.api.nvim_buf_get_text(0, node_row, node_col, node_row, node_col + 1, {})[1]
-		vim.pretty_print("check", char)
 		if opening_parens[char] ~= nil then
 			local sibling_node = node:prev_sibling()
-			local sibling_row, sibling_col, _ = sibling_node:start()
-			local parent_node = node:parent()
-			local parent_row, parent_col, _ = parent_node:start()
-			vim.pretty_print("parent", parent_row, parent_col, "sibling", sibling_row, sibling_col)
-			if parent_row ~= sibling_row or parent_col < sibling_col - 1 then
-				vim.api.nvim_buf_set_text(0, node_row, node_col, node_row, node_col + 1, { "" })
-				vim.api.nvim_buf_set_text(0, sibling_row, sibling_col, sibling_row, sibling_col, { char })
+			if sibling_node then
+				local sibling_row, sibling_col, _ = sibling_node:start()
+				local parent_node = node:parent()
+				local parent_row, parent_col, _ = parent_node:start()
+				if parent_row ~= sibling_row or parent_col < sibling_col - 1 then
+					vim.api.nvim_buf_set_text(0, node_row, node_col, node_row, node_col + 1, { "" })
+					vim.api.nvim_buf_set_text(0, sibling_row, sibling_col, sibling_row, sibling_col, { char })
+					local ns_id =
+						vim.api.nvim_buf_add_highlight(0, 0, M.default.hl, sibling_row, sibling_col, sibling_col + 1)
+					vim.defer_fn(function()
+						vim.api.nvim_buf_clear_namespace(0, ns_id, sibling_row, sibling_row + 1)
+					end, 250)
+				end
+				return
 			end
-			return
 		end
 		node = node:parent()
 	end
 end
 
-vim.keymap.set({ "n", "i" }, "<F7>", function()
-	M.backward_slurp()
-end, {})
-vim.keymap.set({ "n", "i" }, "<F8>", function()
-	M.forward_slurp()
-end, {})
+-- forward barf
+M.forward_barf = function()
+	local cur = vim.api.nvim_win_get_cursor(0)
+	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
+	while node do
+		local node_row, node_col, _ = node:end_()
+		local char = vim.api.nvim_buf_get_text(0, node_row, node_col - 1, node_row, node_col, {})[1]
+		if closing_parens[char] then
+			local child_count = node:child_count()
+			local child_node = node:child(math.max(0, child_count - 3)) --  first and last is paren
+			if child_node then
+				local child_row, child_col, _ = child_node:end_()
+				vim.api.nvim_buf_set_text(0, node_row, node_col - 1, node_row, node_col, { "" })
+				vim.api.nvim_buf_set_text(0, child_row, child_col, child_row, child_col, { char })
+				local ns_id = vim.api.nvim_buf_add_highlight(0, 0, "incsearch", child_row, child_col, child_col + 1)
+				vim.defer_fn(function()
+					vim.api.nvim_buf_clear_namespace(0, ns_id, child_row, child_row + 1)
+				end, 250)
+				return
+			end
+		end
+		node = node:parent()
+	end
+end
+
+-- backward barf
+M.backward_barf = function()
+	local cur = vim.api.nvim_win_get_cursor(0)
+	local node = vim.treesitter.get_node_at_pos(0, cur[1] - 1, cur[2], {})
+	while node do
+		local node_row, node_col, _ = node:start()
+		local char = vim.api.nvim_buf_get_text(0, node_row, node_col, node_row, node_col + 1, {})[1]
+		if opening_parens[char] then
+			local child_count = node:child_count()
+			local child_node = node:child(math.min(child_count, 2)) --  first and last is paren
+			if child_node then
+				local child_row, child_col, _ = child_node:start()
+				vim.api.nvim_buf_set_text(0, node_row, node_col, node_row, node_col + 1, { "" })
+				vim.api.nvim_buf_set_text(0, child_row, child_col - 1, child_row, child_col - 1, { char })
+				local ns_id = vim.api.nvim_buf_add_highlight(0, 0, "IncSearch", child_row, child_col - 1, child_col)
+				vim.defer_fn(function()
+					vim.api.nvim_buf_clear_namespace(0, ns_id, child_row, child_row + 1)
+				end, 250)
+				return
+			end
+		end
+		node = node:parent()
+	end
+end
+
+-- vim.keymap.set({ "n", "i" }, "<F7>", function()
+-- 	M.raise_node()
+-- end, {})
+-- vim.keymap.set({ "n", "i" }, "<F8>", function()
+-- 	M.backward_barf()
+-- end, {})
 
 return M
